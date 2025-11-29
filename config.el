07d07b66d83927b9a5cb6efbe749a91b87d612ee
pandalanax <oskar.g@posteo.net>
2025-11-29 15:49:49 +0100

    change to gruvbox

--- config.el
+++ config.el
@@ -34,7 +34,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'everforest-hard-dark)
+(setq doom-theme 'doom-gruvbox)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -98,3 +98,5 @@
 
 (global-aggressive-indent-mode 1)
 (add-to-list 'aggressive-indent-excluded-modes 'html-mode)
+
+
