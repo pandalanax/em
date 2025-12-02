d92dd5f87623e95b1e4c1057063f91695d2911bf
pandalanax <oskar.g@posteo.net>
2025-12-02 07:41:22 +0100

    fix: hopefully speed up tramp

--- config.el
+++ config.el
@@ -147,3 +147,13 @@
   :config
   ;; Ensure icons show immediately
   (setq nerd-icons-dired-mode t))
+
+(after! tramp
+  (setq tramp-inline-compress-start-size 1000)
+  (setq tramp-copy-size-limit 10000)
+  (setq vc-handled-backends '(Git))
+  (setq tramp-verbose 1)
+  (setq tramp-default-method "scp")
+  (setq tramp-use-ssh-controlmaster-options nil)
+  (setq projectile--mode-line "Projectile")
+  (setq tramp-verbose 1))
